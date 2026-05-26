17212ffea0e8342eedde7e17938107ee7f61af61
DenebTs1 <ts1.dev@icloud.com>
2026-05-26 14:39:31 +0200

    Initial Commit

---
+++ Il2ccpDumper/Il2ccpDumper.h
@@ -0,0 +1,12 @@
+//
+//  Il2ccpDumper.h
+//  Il2ccpDumper
+//
+//  Created by ts1 on 26/05/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Il2ccpDumper : NSObject
+
+@end
---
+++ Il2ccpDumper/Il2ccpDumper.m
@@ -0,0 +1,12 @@
+//
+//  Il2ccpDumper.m
+//  Il2ccpDumper
+//
+//  Created by ts1 on 26/05/2026.
+//
+
+#import "Il2ccpDumper.h"
+
+@implementation Il2ccpDumper
+
+@end
